e007c3df350aeb802bd0c653d5dc08e50d3fa9fe
Charles Dustin Smith <charlesdsmith19@gmail.com>
2023-07-08 21:04:13 -0230

    Added WPF_11C (Playing around with checkbox)

---
+++ WPF_11C/MainWindow.xaml.cs
@@ -0,0 +1,50 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows;
+using System.Windows.Controls;
+using System.Windows.Data;
+using System.Windows.Documents;
+using System.Windows.Input;
+using System.Windows.Media;
+using System.Windows.Media.Imaging;
+using System.Windows.Navigation;
+using System.Windows.Shapes;
+
+namespace WPF_11C
+{
+    /// <summary>
+    /// Interaction logic for MainWindow.xaml
+    /// </summary>
+    public partial class MainWindow : Window
+    {
+        public MainWindow()
+        {
+            InitializeComponent();
+        }
+
+        private void cbAllCheckedChanged(object sender, RoutedEventArgs e)
+        {
+            bool newVal = (cbAllToppings.IsChecked == true);
+            cbSalami.IsChecked = newVal;
+            cbMushrooms.IsChecked = newVal;
+            cbMozzarella.IsChecked = newVal;
+        }
+
+        private void cbSingleCheckedChanged(object sender, RoutedEventArgs e)
+        {
+            cbAllToppings.IsChecked = null;
+            if((cbMozzarella.IsChecked ==  true) && (cbSalami.IsChecked == true) && (cbMushrooms.IsChecked == true))
+            {
+                cbAllToppings.IsChecked = true;
+            }
+
+            if ((cbMozzarella.IsChecked == false) && (cbSalami.IsChecked == false) && (cbMushrooms.IsChecked == false))
+            {
+                cbAllToppings.IsChecked = false;
+            }
+        }
+    }
+}
