ae4ef555cb1cd04a1be64e5345c45c84894a9345
Charles Dustin Smith <charlesdsmith19@gmail.com>
2023-07-10 20:01:28 -0230

    Added EnvironmentVariable, example of how to set and get environment variabel

---
+++ EnvironmentVariable/Program.cs
@@ -0,0 +1,34 @@
+﻿// See https://aka.ms/new-console-template for more information
+using System; 
+
+namespace EnvironmentVariable
+{
+    internal class Program
+
+    {
+        static void Main(string[] args)
+        {
+            System.Console.WriteLine("Hello World!");
+        }
+
+        static void SetEnvironmentVariable()
+        {
+            // Set an environment variable
+            string variableName = "MY_VARIABLE";
+            string variableValue = "my value";
+            Environment.SetEnvironmentVariable(variableName, variableValue, EnvironmentVariableTarget.Process);
+
+            // Read an environment variable
+            string variableValue = Environment.GetEnvironmentVariable(variableName);
+            if (variableValue != null)
+            {
+                Console.WriteLine($"The value of {variableName} is {variableValue}");
+            }
+            else
+            {
+                Console.WriteLine($"The environment variable {variableName} is not set");
+            }
+
+        }
+    }
+}
