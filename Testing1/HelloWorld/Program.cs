722ed63076b980a79a8b79c9f714e774e85017eb
Charles Dustin Smith <cdsmith@mun.ca>
2023-08-08 22:29:13 -0230

    Creating a X11 window using C#

--- Testing1/HelloWorld/Program.cs
+++ Testing1/HelloWorld/Program.cs
@@ -1,28 +1,123 @@
 ﻿// See https://aka.ms/new-console-template for more information
 // This is the new way of doing a simple hello world.
 // Console.WriteLine("Hello, World!");
-
 using System;
+using X11;
 
-namespace HelloWorld
+class Program
 {
-    class Program
+    static IntPtr display;
+    static IntPtr window;
+
+    static void Main(string[] args)
     {
-        static void Main(string[] args)
+        display = XOpenDisplay(IntPtr.Zero);
+        if (display == IntPtr.Zero)
         {
-            /* Entry Point to our program */
-            ConsoleColor currentColor = Console.ForegroundColor;
-            Console.ForegroundColor = ConsoleColor.Cyan;
-
-            Console.Write("Hello World! :)\n");
-            
-            Console.ForegroundColor = ConsoleColor.Green;
-            Console.WriteLine("Hello Suck My Balls! :P");
-
-            
-            Console.Read();
-
-            Console.ForegroundColor = currentColor;
+            Console.WriteLine("Failed to open X11 display.");
+            return;
         }
+
+        int screen = XDefaultScreen(display);
+        IntPtr rootWindow = XRootWindow(display, screen);
+
+        int windowWidth = 400;
+        int windowHeight = 300;
+
+        window = XCreateSimpleWindow(
+            display, rootWindow,
+            0, 0, windowWidth, windowHeight,
+            0, XBlackPixel(display, screen), XWhitePixel(display, screen)
+        );
+
+        XSelectInput(display, window, XEventMask.ExposureMask | XEventMask.KeyPressMask);
+        XMapWindow(display, window);
+
+        while (true)
+        {
+            XNextEvent(display, out XEvent xEvent);
+            switch (xEvent.type)
+            {
+                case XEventType.Expose:
+                    // Handle expose event (window needs to be redrawn)
+                    break;
+                case XEventType.KeyPress:
+                    // Handle key press event
+                    Console.WriteLine("Key pressed!");
+                    break;
+            }
+        }
+    }
+
+    static void Cleanup()
+    {
+        XDestroyWindow(display, window);
+        XCloseDisplay(display);
+    }
+
+    // Import Xlib functions using P/Invoke
+    [DllImport("libX11", EntryPoint = "XOpenDisplay")]
+    public static extern IntPtr XOpenDisplay(IntPtr display);
+
+    [DllImport("libX11", EntryPoint = "XDefaultScreen")]
+    public static extern int XDefaultScreen(IntPtr display);
+
+    [DllImport("libX11", EntryPoint = "XRootWindow")]
+    public static extern IntPtr XRootWindow(IntPtr display, int screen);
+
+    [DllImport("libX11", EntryPoint = "XBlackPixel")]
+    public static extern ulong XBlackPixel(IntPtr display, int screen);
+
+    [DllImport("libX11", EntryPoint = "XWhitePixel")]
+    public static extern ulong XWhitePixel(IntPtr display, int screen);
+
+    [DllImport("libX11", EntryPoint = "XCreateSimpleWindow")]
+    public static extern IntPtr XCreateSimpleWindow(
+        IntPtr display, IntPtr parent,
+        int x, int y, uint width, uint height,
+        uint border_width, ulong border, ulong background
+    );
+
+    [DllImport("libX11", EntryPoint = "XSelectInput")]
+    public static extern int XSelectInput(IntPtr display, IntPtr window, XEventMask eventMask);
+
+    [DllImport("libX11", EntryPoint = "XMapWindow")]
+    public static extern void XMapWindow(IntPtr display, IntPtr window);
+
+    [DllImport("libX11", EntryPoint = "XNextEvent")]
+    public static extern void XNextEvent(IntPtr display, out XEvent event_return);
+
+    [DllImport("libX11", EntryPoint = "XDestroyWindow")]
+    public static extern void XDestroyWindow(IntPtr display, IntPtr window);
+
+    [DllImport("libX11", EntryPoint = "XCloseDisplay")]
+    public static extern int XCloseDisplay(IntPtr display);
+
+    // Define XEventMask enum
+    [Flags]
+    public enum XEventMask : long
+    {
+        NoEventMask = 0,
+        KeyPressMask = 1 << 0,
+        KeyReleaseMask = 1 << 1,
+        ExposureMask = 1 << 15,
+    }
+
+    // Define XEvent struct
+    [StructLayout(LayoutKind.Explicit)]
+    public struct XEvent
+    {
+        [FieldOffset(0)]
+        public XEventType type;
+
+        // Define other fields as needed based on event type
+    }
+
+    // Define XEventType enum
+    public enum XEventType
+    {
+        Expose,
+        KeyPress,
+        // Define other event types as needed
     }
 }
