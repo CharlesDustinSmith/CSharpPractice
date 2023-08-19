62da7fa189d1ef6c4ad0e4ada0fa58c4fbcbd0b1
Charles Dustin Smith <cdsmith@mun.ca>
2023-08-19 09:30:43 -0230

    Created AVL Tree and B* Tree for C#

---
+++ AVLTree/Program.cs
@@ -0,0 +1,161 @@
+﻿// See https://aka.ms/new-console-template for more information
+using System;
+
+class Node
+{
+    public int Data;
+    public int Height;
+    public Node Left;
+    public Node Right;
+
+    public Node(int data)
+    {
+        Data = data;
+        Height = 1;
+        Left = null;
+        Right = null;
+    }
+}
+
+class AVLTree
+{
+    public Node Root;
+
+    public AVLTree()
+    {
+        Root = null;
+    }
+
+    private int GetHeight(Node node)
+    {
+        if (node == null)
+            return 0;
+        return node.Height;
+    }
+
+    private int GetBalanceFactor(Node node)
+    {
+        if (node == null)
+            return 0;
+        return GetHeight(node.Left) - GetHeight(node.Right);
+    }
+
+    private Node RotateRight(Node y)
+    {
+        Node x = y.Left;
+        Node T2 = x.Right;
+
+        x.Right = y;
+        y.Left = T2;
+
+        y.Height = Math.Max(GetHeight(y.Left), GetHeight(y.Right)) + 1;
+        x.Height = Math.Max(GetHeight(x.Left), GetHeight(x.Right)) + 1;
+
+        return x;
+    }
+
+    private Node RotateLeft(Node x)
+    {
+        Node y = x.Right;
+        Node T2 = y.Left;
+
+        y.Left = x;
+        x.Right = T2;
+
+        x.Height = Math.Max(GetHeight(x.Left), GetHeight(x.Right)) + 1;
+        y.Height = Math.Max(GetHeight(y.Left), GetHeight(y.Right)) + 1;
+
+        return y;
+    }
+
+    public void Insert(int data)
+    {
+        Root = InsertRec(Root, data);
+    }
+
+    private Node InsertRec(Node root, int data)
+    {
+        if (root == null)
+        {
+            return new Node(data);
+        }
+
+        if (data < root.Data)
+        {
+            root.Left = InsertRec(root.Left, data);
+        }
+        else if (data > root.Data)
+        {
+            root.Right = InsertRec(root.Right, data);
+        }
+        else // Duplicate data not allowed
+        {
+            return root;
+        }
+
+        root.Height = 1 + Math.Max(GetHeight(root.Left), GetHeight(root.Right));
+
+        int balance = GetBalanceFactor(root);
+
+        // Left Left Case
+        if (balance > 1 && data < root.Left.Data)
+        {
+            return RotateRight(root);
+        }
+
+        // Right Right Case
+        if (balance < -1 && data > root.Right.Data)
+        {
+            return RotateLeft(root);
+        }
+
+        // Left Right Case
+        if (balance > 1 && data > root.Left.Data)
+        {
+            root.Left = RotateLeft(root.Left);
+            return RotateRight(root);
+        }
+
+        // Right Left Case
+        if (balance < -1 && data < root.Right.Data)
+        {
+            root.Right = RotateRight(root.Right);
+            return RotateLeft(root);
+        }
+
+        return root;
+    }
+
+    public void InOrderTraversal(Node root)
+    {
+        if (root != null)
+        {
+            InOrderTraversal(root.Left);
+            Console.Write(root.Data + " ");
+            InOrderTraversal(root.Right);
+        }
+    }
+
+    public void Display()
+    {
+        InOrderTraversal(Root);
+    }
+}
+
+class Program
+{
+    static void Main(string[] args)
+    {
+        AVLTree tree = new AVLTree();
+        tree.Insert(10);
+        tree.Insert(20);
+        tree.Insert(30);
+        tree.Insert(40);
+        tree.Insert(50);
+        tree.Insert(25);
+
+        Console.WriteLine("In-order traversal of AVL tree:");
+        tree.Display();
+    }
+}
+
---
+++ BStarTree/Program.cs
@@ -0,0 +1,78 @@
+﻿// See https://aka.ms/new-console-template for more information
+using System;
+using System.Collections.Generic;
+
+class BStarTreeNode
+{
+    public List<int> Keys;
+    public List<BStarTreeNode> Children;
+    public BStarTreeNode Parent;
+    public bool IsLeaf;
+
+    public BStarTreeNode()
+    {
+        Keys = new List<int>();
+        Children = new List<BStarTreeNode>();
+        Parent = null;
+        IsLeaf = true;
+    }
+}
+
+class BStarTree
+{
+    private BStarTreeNode Root;
+    private int Degree;
+
+    public BStarTree(int degree)
+    {
+        Root = new BStarTreeNode();
+        Degree = degree;
+    }
+
+    public void Insert(int key)
+    {
+        // Implement insertion logic here
+    }
+
+    public void Display()
+    {
+        InOrderTraversal(Root);
+    }
+
+    private void InOrderTraversal(BStarTreeNode node)
+    {
+        if (node != null)
+        {
+            int i;
+            for (i = 0; i < node.Keys.Count; i++)
+            {
+                if (!node.IsLeaf)
+                {
+                    InOrderTraversal(node.Children[i]);
+                }
+                Console.Write(node.Keys[i] + " ");
+            }
+            if (!node.IsLeaf)
+            {
+                InOrderTraversal(node.Children[i]);
+            }
+        }
+    }
+}
+
+class Program
+{
+    static void Main(string[] args)
+    {
+        BStarTree tree = new BStarTree(3); // Example degree of 3
+        tree.Insert(10);
+        tree.Insert(20);
+        tree.Insert(5);
+        tree.Insert(15);
+        tree.Insert(25);
+
+        Console.WriteLine("In-order traversal of B* tree:");
+        tree.Display();
+    }
+}
+
